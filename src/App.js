e9b688ffa4edc29c6aaf68c973a60a1ee6acbc51
Fernetazo <angelofdarkness88@gmail.com>
2022-03-09 20:39:17 -0300

    Section one setState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,71 @@
-function App() {
-  return <div>Hello</div>;
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: "",
+      email: "",
+      phone: "",
+    };
+  }
+
+  handleNameChange = (e) => {
+    this.setState({
+      name: e.target.value,
+    });
+  };
+
+  handleEmailChange = (e) => {
+    this.setState({
+      email: e.target.value,
+    });
+  };
+
+  handlePhoneChange = (e) => {
+    this.setState({
+      phone: e.target.value,
+    });
+  };
+
+  onSubmitCV = (e) => {
+    e.preventDefault();
+  };
+
+  render() {
+    const { name, email, phone } = this.state;
+    return (
+      <div>
+        <form onSubmit={this.onSubmitCV}>
+          <div className="sectionOne">
+            <label>Name:</label>
+            <input
+              value={name}
+              type="text"
+              onChange={this.handleNameChange}
+            ></input>
+            <br></br>
+            <label>E-mail:</label>
+            <input
+              value={email}
+              type="email"
+              onChange={this.handleEmailChange}
+            ></input>
+            <br></br>
+            <label>Phone:</label>
+            <input
+              value={phone}
+              type="number"
+              onChange={this.handlePhoneChange}
+            ></input>
+          </div>
+          <br></br>
+          <button type="submit">Submit CV</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
