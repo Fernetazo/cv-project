265ce7cc503a952142ca5a2c1551e82a30268369
Fernetazo <angelofdarkness88@gmail.com>
2022-03-10 11:28:10 -0300

    Correctly add setState without bugs

--- src/App.js
+++ src/App.js
@@ -5,88 +5,164 @@ class App extends Component {
     super();
 
     this.state = {
-      name: "",
-      email: "",
-      phone: "",
+      personalInfo: { name: "", email: "", phone: "" },
 
-      institution: "",
-      title: "",
-      titleDate: "",
+      eduInfo: { institution: "", title: "", titleDate: "" },
 
-      company: "",
-      position: "",
-      tasks: "",
-      dateFrom: "",
-      dateTo: "",
+      expInfo: {
+        company: "",
+        position: "",
+        tasks: "",
+        dateFrom: "",
+        dateTo: "",
+      },
 
-      educationInfo: [],
-      experience: [],
+      educationArray: [],
+      experienceArray: [],
     };
   }
 
   handleNameChange = (e) => {
     this.setState({
-      name: e.target.value,
+      personalInfo: {
+        name: e.target.value,
+        email: this.state.personalInfo.email,
+        phone: this.state.personalInfo.phone,
+      },
     });
   };
 
   handleEmailChange = (e) => {
     this.setState({
-      email: e.target.value,
+      personalInfo: {
+        email: e.target.value,
+        name: this.state.personalInfo.name,
+        phone: this.state.personalInfo.phone,
+      },
     });
   };
 
   handlePhoneChange = (e) => {
     this.setState({
-      phone: e.target.value,
+      personalInfo: {
+        phone: e.target.value,
+        name: this.state.personalInfo.name,
+        email: this.state.personalInfo.email,
+      },
     });
   };
 
   handleInstitutionChange = (e) => {
     this.setState({
-      institution: e.target.value,
+      eduInfo: {
+        institution: e.target.value,
+        title: this.state.eduInfo.title,
+        titleDate: this.state.eduInfo.titleDate,
+      },
     });
   };
 
   handleTitleChange = (e) => {
     this.setState({
-      title: e.target.value,
+      eduInfo: {
+        title: e.target.value,
+        institution: this.state.eduInfo.institution,
+        titleDate: this.state.eduInfo.titleDate,
+      },
     });
   };
 
   handleTitleDateChange = (e) => {
     this.setState({
-      titleDate: e.target.value,
+      eduInfo: {
+        titleDate: e.target.value,
+        institution: this.state.eduInfo.institution,
+        title: this.state.eduInfo.title,
+      },
     });
   };
 
   handleCompanyChange = (e) => {
     this.setState({
-      company: e.target.value,
+      expInfo: {
+        company: e.target.value,
+        position: this.state.expInfo.position,
+        tasks: this.state.expInfo.tasks,
+        dateFrom: this.state.expInfo.dateFrom,
+        dateTo: this.state.expInfo.dateTo,
+      },
     });
   };
 
   handlePositionChange = (e) => {
     this.setState({
-      position: e.target.value,
+      expInfo: {
+        position: e.target.value,
+        company: this.state.expInfo.company,
+        tasks: this.state.expInfo.tasks,
+        dateFrom: this.state.expInfo.dateFrom,
+        dateTo: this.state.expInfo.dateTo,
+      },
     });
   };
 
   handleTasksChange = (e) => {
     this.setState({
-      tasks: e.target.value,
+      expInfo: {
+        tasks: e.target.value,
+        company: this.state.expInfo.company,
+        position: this.state.expInfo.position,
+        dateFrom: this.state.expInfo.dateFrom,
+        dateTo: this.state.expInfo.dateTo,
+      },
     });
   };
 
   handleDateFromChange = (e) => {
     this.setState({
-      dateFrom: e.target.value,
+      expInfo: {
+        dateFrom: e.target.value,
+        company: this.state.expInfo.company,
+        position: this.state.expInfo.position,
+        tasks: this.state.expInfo.tasks,
+        dateTo: this.state.expInfo.dateTo,
+      },
     });
   };
 
   handleDateToChange = (e) => {
     this.setState({
-      dateTo: e.target.value,
+      expInfo: {
+        dateTo: e.target.value,
+        company: this.state.expInfo.company,
+        position: this.state.expInfo.position,
+        tasks: this.state.expInfo.tasks,
+        dateFrom: this.state.expInfo.dateFrom,
+      },
+    });
+  };
+
+  addEdu = () => {
+    this.setState({
+      educationArray: this.state.educationArray.concat(this.state.eduInfo),
+      eduInfo: {
+        institution: "",
+        title: "",
+        titleDate: "",
+      },
+    });
+  };
+
+  addExp = () => {
+    this.setState({
+      experienceArray: this.state.experienceArray.concat(this.state.expInfo),
+      expInfo: {
+        company: "",
+        position: "",
+        tasks: "",
+        dateFrom: "",
+        dateTo: "",
+      },
     });
   };
 
@@ -95,109 +171,103 @@ class App extends Component {
   };
 
   render() {
-    const {
-      name,
-      email,
-      phone,
-      institution,
-      title,
-      titleDate,
-      company,
-      position,
-      tasks,
-      dateFrom,
-      dateTo,
-      educationInfo,
-      experience,
-    } = this.state;
-
+    const { personalInfo, eduInfo, expInfo, educationArray, experienceArray } =
+      this.state;
     return (
       <div>
         <form onSubmit={this.onSubmitCV}>
-          <div className="sectionOne">
-            <label>Name:</label>
+          <div className="section">
+            <label htmlFor="nameInput">Name:</label>
             <input
-              value={name}
               type="text"
               onChange={this.handleNameChange}
+              value={personalInfo.name}
+              id="nameInput"
             ></input>
             <br></br>
             <label>E-mail:</label>
             <input
-              value={email}
+              value={personalInfo.email}
               type="email"
               onChange={this.handleEmailChange}
             ></input>
             <br></br>
             <label>Phone:</label>
             <input
-              value={phone}
+              value={personalInfo.phone}
               type="number"
               onChange={this.handlePhoneChange}
             ></input>
           </div>
           <br></br>
 
-          <div className="sectionTwo">
+          <div className="section">
             <label>Institution:</label>
             <input
-              value={institution}
+              value={eduInfo.institution}
               type="text"
               onChange={this.handleInstitutionChange}
             ></input>
             <br></br>
             <label>Title:</label>
             <input
-              value={title}
+              value={eduInfo.title}
               type="text"
               onChange={this.handleTitleChange}
             ></input>
             <br></br>
             <label>Title date:</label>
             <input
-              value={titleDate}
+              value={eduInfo.titleDate}
               type="date"
               onChange={this.handleTitleDateChange}
             ></input>
+            <br></br>
+            <button onClick={this.addEdu}>Add education info</button>
           </div>
           <br></br>
 
-          <div className="sectionThree">
+          <div className="section">
             <label>Company name:</label>
             <input
-              value={company}
+              value={expInfo.company}
               type="text"
               onChange={this.handleCompanyChange}
             ></input>
             <br></br>
             <label>Position title:</label>
             <input
-              value={position}
+              value={expInfo.position}
               type="text"
               onChange={this.handlePositionChange}
             ></input>
             <br></br>
             <label>Tasks description:</label>
             <input
-              value={tasks}
+              value={expInfo.tasks}
               type="text"
               onChange={this.handleTasksChange}
             ></input>
+            <br></br>
             <label>Date from:</label>
             <input
-              value={dateFrom}
+              value={expInfo.dateFrom}
               type="date"
               onChange={this.handleDateFromChange}
             ></input>
+            <br></br>
             <label>Date to:</label>
             <input
-              value={dateTo}
+              value={expInfo.dateTo}
               type="date"
               onChange={this.handleDateToChange}
             ></input>
+            <br></br>
+            <button onClick={this.addExp}>Add experience info</button>
           </div>
           <br></br>
-          <button type="submit">Submit CV</button>
+          <br></br>
+          <button type="submit">SUBMIT CV</button>
         </form>
       </div>
     );
